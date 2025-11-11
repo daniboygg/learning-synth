97b59454da2306f82c115ca3bd0691c1f61429bf
daniboygg <danigayosog@gmail.com>
2025-11-11 18:13:55 +0100

    Adapt project to main
    * This was migrated from another project, now is standalone

---
+++ waves.c
@@ -0,0 +1,49 @@
+/*
+    Wave functions, checks desmos for the plot of the signals genrated
+    https://www.desmos.com/calculator/eugiiu4iky
+*/
+#include <SDL3/SDL.h>
+#include <assert.h>
+
+typedef enum {
+    WAVE_SINE,
+    WAVE_SQUARE,
+    WAVE_SAW,
+    WAVE_TRIANGLE,
+} WavesType;
+
+
+char *waves_type_to_str(WavesType t) {
+    switch (t) {
+        case WAVE_SINE:
+            return "SINE";
+        case WAVE_SQUARE:
+            return "SQUARE";
+        case WAVE_SAW:
+            return "SAW";
+        case WAVE_TRIANGLE:
+            return "TRIANGLE";
+        default:
+            assert(false);
+    }
+}
+
+float waves_sine(float amplitude, float phase) {
+    float y = amplitude * SDL_sinf(2 * SDL_PI_F * phase);
+    return y;
+}
+
+float waves_square(float amplitude, float phase) {
+    float y = amplitude * (SDL_sinf(2 * SDL_PI_F * phase) > 0 ? 1.0f : -1.0f);
+    return y;
+}
+
+float waves_saw(float amplitude, float phase) {
+    float y = -amplitude + 2 * amplitude * phase;
+    return y;
+}
+
+float waves_triangle(float amplitude, float phase) {
+    float y = amplitude - 4 * amplitude * SDL_fabsf(phase - 0.5);
+    return y;
+}
